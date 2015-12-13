c2952de27116e21118353c553d1fb9d6ebfeea74
weiyu.lin <weiyu.lin@rakuten.com>
2015-12-13 15:27:34 +0800

    rolling back

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 var fs = require('fs');
 var reactTemplates=require("react-templates/src/reactTemplates");
-var requireFromString = require('require-from-string');
 
 var installed = false;
-
-function installType(opt){
-  if(opt===null || opt.type === "file"){
-    install();
-  }else{
-    return compileWithString;
-  }
-}
 
 function install() {
   if (installed) {
@@ -25,7 +16,7 @@ function install() {
       source = reactTemplates.convertTemplateToReact(source,{modules: 'commonjs', name: 'template'});
 
     } catch (e) {
-      throw new Error('Error transforming ' + filename + ' to React: ' + e.toString());
+      throw new Error('Error transforming ' + filename + ' to JS: ' + e.toString());
     }
     module._compile(source, filename);
   };
@@ -33,19 +24,6 @@ function install() {
   installed = true;
 }
 
-function compileWithString (source, opt) {
-  if(opt === null || {}){
-    opt = {modules: 'commonjs', name: 'template'};
-  }
-  try{
-    source = reactTemplates.convertTemplateToReact(source, opt);
-    var component = requireFromString(source);
-    return component;
-  } catch(e) {
-    throw new Error('Error transforming ' + source + ' to React: ' + e.toString());
-  }
-}
-
 module.exports = {
-  install: installType
+  install: install
 };
