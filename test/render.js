dfcaae7945cdbc0217dda5aecc22401144331986
weiyu.lin <weiyu.lin@rakuten.com>
2015-12-13 15:47:20 +0800

    add mocha tests

---
+++ test/render.js
@@ -0,0 +1,11 @@
+var React = require('react/addons');
+	require('node-jsx').install();
+	require('../index').install();
+
+module.exports=function render(componentFilePath) {
+	// body...
+	var component = require(componentFilePath);
+	var contents = React.renderToStaticMarkup(React.createElement(component));
+	console.log(contents);
+	return contents;
+}
